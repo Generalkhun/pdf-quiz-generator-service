7d3e2012c1238593e28fa93d361ef4a9f049d122
Generalkhun <khun2551@hotmail.com>
2020-12-10 18:46:10 +0700

    Add adding question answwering

--- src/AnswerSheetInput.js
+++ src/AnswerSheetInput.js
@@ -1,31 +1,67 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const addAnswer = () => {
-  console.log("Add answer")
+  console.log("Add answer");
+};
 
-}
-
-export default function(){
+export default function () {
   const [type, setType] = useState("SELECT");
-  
-  const [allAns, setAllAns] = useState({})
+  const [allAns, setAllAns] = useState(<div></div>);
+  const [choiceNumInput, setChoiceNumInput] = useState(4);
+  useEffect(() => {
+    console.log("add question");
+  }, allAns);
   return (
     <div>
-      <p>{type}</p>
       <button
         onClick={() => {
-            setType(prev => {
-                let newState = "SELECT"
-                if (prev === "SELECT") {newState = "FILL"}
-                return newState
-            })
+          setType((prev) => {
+            let newState = "SELECT";
+            if (prev === "SELECT") {
+              newState = "FILL";
+            }
+            return newState;
+          });
         }}
       >
         Change answer type
       </button>
-      <button onClick = {() => {addAnswer()}}>Add</button>
+      <button
+        onClick={() => {
+          addAnswer();
+        }}
+      >
+        Add
+      </button>
+      <div>
+        <p>Question type: {type}</p>
+        {type === "SELECT" ? (
+          <div>
+            <button
+              onClick={() => {
+                setChoiceNumInput((prev) => prev + 1);
+              }}
+            >
+              +
+            </button>
+            <p>Choices : {choiceNumInput}</p>
+
+            <button
+              onClick={() => {
+                setChoiceNumInput((prev) => prev - 1);
+              }}
+            >
+              -
+            </button>
+          </div>
+        ) : (
+          <div>
+            <p>Answer</p>
+            <input type="text" name="choiceAns" />
+          </div>
+        )}
+      </div>
+      <div>{allAns}</div>
     </div>
   );
-};
-
-
+}
